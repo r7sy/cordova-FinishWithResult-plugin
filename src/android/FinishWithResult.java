028466a0398e0af8391a5cf6eb7dd4b1b0553d00
r7sy <sy-vip@live.com>
2017-01-10 14:28:04 +0200

    Added plugin logic

---
+++ src/android/FinishWithResult.java
@@ -0,0 +1,40 @@
+package r7sy.FinishWithResult;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Intent;
+
+import android.content.Context;
+import android.net.Uri;
+import android.app.Activity;
+
+
+public class FinishWithResult extends CordovaPlugin {
+
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+           try {
+                
+				Uri uri = Uri.parse(args.getString(0));
+              Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND, uri);
+            this.cordova.getActivity().setResult(Activity.RESULT_OK, shareIntent); 
+           this.cordova.getActivity().finish(); 
+               
+
+                return true;
+            } catch (Exception e) {
+                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, e.toString()));
+            }
+       
+
+        return false;
+    }
+}
+
+
